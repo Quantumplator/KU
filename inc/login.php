ad6a181d8ed7b9f507fa48b68df6028a1e76592a
Dylan J Harris <dylansbook@gmail.com>
2015-09-22 21:42:06 -0700

    Login css is now editable in the forms partials

--- inc/login.php
+++ inc/login.php
@@ -12,6 +12,6 @@ add_filter('login_headerurl', '_ku_url_login');
  * Change login screen icon
  */
 function _ku_login_css() {
-  wp_enqueue_style( 'login_css', get_template_directory_uri() . 'style.css' );
+  wp_enqueue_style( 'login_css', get_template_directory_uri() . '/style.css' );
 }
 add_action('login_head', '_ku_login_css');
