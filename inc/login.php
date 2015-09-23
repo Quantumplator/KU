88a11dc20ae826a8ee5490be8eb1181713dcee67
Dylan J Harris <dylansbook@gmail.com>
2015-09-22 19:52:16 -0700

    Updating

--- inc/login.php
+++ inc/login.php
@@ -2,16 +2,16 @@
 /**
  * Change login screen url
  */
-function wpc_url_login(){
-  return "http://studio-ku.com/";
+function _ku_url_login(){
+  return site_url();
 }
-add_filter('login_headerurl', 'wpc_url_login');
+add_filter('login_headerurl', '_ku_url_login');
 
 
 /**
  * Change login screen icon
  */
-function login_css() {
+function _ku_login_css() {
   wp_enqueue_style( 'login_css', get_template_directory_uri() . 'style.css' );
 }
-add_action('login_head', 'login_css');
+add_action('login_head', '_ku_login_css');
