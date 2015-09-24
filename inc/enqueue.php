c38395e2e997f692c94de79b2b41c0f0b761c3b0
Dylan J Harris <dylansbook@gmail.com>
2015-09-23 22:02:12 -0700

    Test

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -7,6 +7,8 @@ function _ku_scripts() {
 
   wp_enqueue_script( '_ku-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
+  wp_enqueue_script( '_ku-main-min', get_template_directory_uri() . '/js/main.min.js', array(), '20120206', true );
+
   wp_enqueue_script( '_ku-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
---
+++ page-landing.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Ku Peninsula
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Studio_Ku
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+      <div class="ku-above"></div>
+      <div class="ku-below">
+        <img 
+          class="ambassador zombneck"
+          src="http://studio-ku.com/wp-content/themes/studioku/img/ambassador-zomb-neck.png" 
+          alt="Ambassador Zomb Neck" 
+          title="#studioku">
+        <img 
+          class="ambassador zombjacket"
+          src="http://studio-ku.com/wp-content/themes/studioku/img/ambassador-zomb-jacket.png" 
+          alt="Ambassador Zomb Jacket" 
+          title="#studioku">
+        <img 
+          class="ambassador zombhead"
+          src="http://studio-ku.com/wp-content/themes/studioku/img/ambassador-zomb-head.png" 
+          alt="Ambassador Zomb Head" 
+          title="#studioku">
+        <img 
+          class="ambassador zombeye"
+          src="http://studio-ku.com/wp-content/themes/studioku/img/ambassador-zomb-eye.png" 
+          alt="Ambassador Zomb Eye" 
+          title="#studioku">
+        <img 
+          class="ambassador zombmouth"
+          src="http://studio-ku.com/wp-content/themes/studioku/img/ambassador-zomb-mouth.png" 
+          alt="Ambassador Zomb Mouth" 
+          title="#studioku">
+      </div>
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
